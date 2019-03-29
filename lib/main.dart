b190dbaaebc699ec47ad4fbdd25f185ec8278051
Satjawat Paanakaan <peanutbutteer@gmail.com>
2019-03-30 02:26:27 +0700

    Add news UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,185 @@
--import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(NewsReaderApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class NewsReaderApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'News',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class News {
+  News({
+    @required this.cover,
+    @required this.title,
+    @required this.content,
+    @required this.createAt,
+  });
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+  DateTime createAt;
+  String cover;
+  String title;
+  String content;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class NewsCard extends StatelessWidget {
+  NewsCard(this.news);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final News news;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              child: SizedBox(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.width * 1.5 / 3,
+                child: Image.network(
+                  news.cover,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20, top: 8),
+            child: Text(
+              news.title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Center(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10,
+                      left: 20,
+                      bottom: 10,
+                    ),
+                    child: Text(news.createAt.toString()),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: () {},
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int pageIndex = 0;
+  List<News> news = List<News>();
+
+  @override
+  void initState() {
+    news.add(News(
+      cover:
+          'https://cdn.theatlantic.com/assets/media/img/mt/2019/03/RTR483UO/lead_720_405.jpg?mod=1553029196',
+      title: 'How Thailand Became the World’s Last Military Dictatorship',
+      content:
+          "If military dictatorship is defined in the strictest sense as the rule of a junta or military officer who comes to power through a coup and then doesn’t hold elections to offer a veneer of legitimacy, then Thailand is the world’s last military dictatorship. \nIt seems difficult to believe that such a peaceful, thriving country that welcomes millions of tourists each year is in fact a military dictatorship, let alone the last one. Yet Thailand has been through so many military coups that they almost have a business-as-usual feel to them. The reality of army rule in the country is that it is, in a political sense, thoroughly unremarkable, reliant on a familiar mix of repression and political control, with one key difference: It has the blessings of a powerful protector.",
+      createAt: DateTime.now(),
+    ));
+    news.add(News(
+      cover:
+          'https://static01.nyt.com/images/2019/03/29/us/politics/29dc-TRUMP-01/merlin_152739000_7365789a-a69f-4a0d-a4a2-0ecba5372db6-superJumbo.jpg?quality=90&auto=webp',
+      title:
+          'Trump Threatens to Close Mexican Border Next Week Over Immigration',
+      content:
+          'Lake Okeechobee, Fla. — President Trump on Friday called on Mexico to stop all illegal immigration, escalating a repeated threat by adding a timeline: Mr. Trump said he would close large swaths or all of the southern border as early as “next week” if the Mexican government did not "immediately” stop all undocumented migrants.',
+      createAt: DateTime.now(),
+    ));
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        title: Text(
+          'Headlines',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+          ),
         ),
+        actions: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(right: 20),
+            child: Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.grey,
+                backgroundImage: NetworkImage(
+                    'https://png.pngtree.com/element_origin_min_pic/16/11/15/89a1f35d71139a37bb3fe572a954f2f8.jpg'),
+              ),
+            ),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        color: Colors.white,
+        child: ListView.builder(
+            itemCount: news.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: NewsCard(news[index]),
+              );
+            }),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.assistant),
+            title: Text('My Feed'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.work),
+            title: Text('Headline'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star_border),
+            title: Text('Favorites'),
+          ),
+        ],
+        onTap: (int index) {
+          setState(() {
+            pageIndex = index;
+          });
+        },
+        currentIndex: pageIndex,
+      ),
     );
   }
 }
